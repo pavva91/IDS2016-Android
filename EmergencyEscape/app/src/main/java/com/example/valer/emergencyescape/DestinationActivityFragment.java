17713c917e3a92ee34383379d44f346e10944f73
pavva91 <valeriomattioli580@gmail.com>
2016-04-21 12:41:50 +0200

    restart 21-04-16

--- EmergencyEscape/app/src/main/java/com/example/valer/emergencyescape/DestinationActivityFragment.java
+++ EmergencyEscape/app/src/main/java/com/example/valer/emergencyescape/DestinationActivityFragment.java
@@ -1,10 +1,12 @@
 package com.example.valer.emergencyescape;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,17 @@ public class DestinationActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_destination, container, false);
+         View myView = inflater.inflate(R.layout.fragment_destination, container, false);
+
+        Button tapBtn = (Button) myView.findViewById(R.id.btnDestinazione);
+        tapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return myView;
     }
 }
