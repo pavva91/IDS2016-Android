618ebbab42623b51e9493ad96fa72d632d0792a8
pavva91 <valeriomattioli580@gmail.com>
2016-05-09 15:55:53 +0200

    spiegazione DAO assistente

---
+++ EmergencyEscape/app/src/main/java/com/emergencyescape/model/Dasdf.java
@@ -0,0 +1,26 @@
+package com.emergencyescape.model;
+
+/**
+ * Created by Valerio Mattioli on 09/05/2016.
+ */
+public interface UserDAO {
+
+    get(String id);
+    create(User user);
+
+}
+
+
+
+MongoUserDAO implements UserDAO
+
+public User get(String id) {
+        jfjyfjy
+        }
+
+
+
+
+        loginActivity(UserDAO dao) {
+        this.dao = dao;
+        }
