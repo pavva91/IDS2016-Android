215871d14c6a33a841a48b829882617ab3161490
pavva91 <valeriomattioli580@gmail.com>
2016-05-12 23:17:24 +0200

    aggiunto UML da Drive

--- EmergencyEscape/app/src/main/java/com/emergencyescape/option/OptionActivity.java
+++ EmergencyEscape/app/src/main/java/com/emergencyescape/option/OptionActivity.java
@@ -4,17 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-import com.emergencyescape.EmQrActivity;
-import com.emergencyescape.EmTapActivity;
-import com.emergencyescape.EmTextActivity;
-import com.emergencyescape.NoEmTapActivity;
-import com.emergencyescape.NoEmTextActivity;
+import com.emergencyescape.EmergencyActivity;
+import com.emergencyescape.NoEmergencyActivity;
 import com.emergencyescape.R;
 
 /**
- * Created by valerio on 27/04/2016.
+ * Created by Valerio Mattioli on 27/04/2016.
  *
  * Activity standard da cui ereditare se si vuole avere l'activity con il menù(em,noem) con i relativi sotto menu
+ *
+ * NB: Esistono solo due Activity che fanno poi da controller sui Fragment da invocare
  */
 
 public class OptionActivity extends AppCompatActivity {
@@ -26,25 +25,31 @@ public class OptionActivity extends AppCompatActivity {
 
         switch (id){
             case R.id.action_emtext:
-                startActivity(new Intent(this,EmTextActivity.class));
+                startActivity(new Intent(this,EmergencyActivity.class).putExtra("fragment", "DepartureTextFragment"));
                 break;
             case R.id.action_emtap:
-                startActivity(new Intent(this,EmTapActivity.class));
+                startActivity(new Intent(this,EmergencyActivity.class).putExtra("fragment", "DepartureTapFragment"));
                 break;
             case R.id.action_emqr:
-                startActivity(new Intent(this,EmQrActivity.class));
+                startActivity(new Intent(this,EmergencyActivity.class).putExtra("fragment", "DepartureQrFragment"));
                 break;
             case R.id.action_noemtext:
-                startActivity(new Intent(this,NoEmTextActivity.class));
+                startActivity(new Intent(this,NoEmergencyActivity.class).putExtra("fragment", "DepartureTextFragment"));
                 break;
             case R.id.action_noemtap:
-                startActivity(new Intent(this,NoEmTapActivity.class));
+                startActivity(new Intent(this,NoEmergencyActivity.class).putExtra("fragment", "DepartureTapFragment"));
                 break;
             case R.id.action_noemqr:
+                startActivity(new Intent(this,NoEmergencyActivity.class).putExtra("fragment", "DepartureQrFragment"));
+
+                /*TODO: Fare Refactoring di questo script che chiama l'intent QRCODE su Presenter QR
                 Intent qrIntent = new Intent("com.google.zxing.client.android.SCAN"); //Intent zxing
                 qrIntent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE" for bar codes
                 startActivityForResult(qrIntent, 0); //start barcode scanner zxing
+                */
+
                 break;
+
 
         }
 
--- EmergencyEscape/app/src/main/java/com/emergencyescape/option/OptionController.java
+++ EmergencyEscape/app/src/main/java/com/emergencyescape/option/OptionController.java
@@ -3,12 +3,6 @@ package com.emergencyescape.option;
  * Created by Valerio Mattioli on 10/05/2016.
  */
 
-import android.content.Intent;
-
-import com.emergencyescape.EmTextActivity;
-
-import static android.support.v4.app.ActivityCompat.startActivity;
-
 /**
  * com.emergencyescape.option
  * OptionController
