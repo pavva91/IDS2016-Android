2026f72afa31ebb3e310c44301623772952959d7
pavva91 <valeriomattioli580@gmail.com>
2016-05-23 10:03:15 +0200

    continuo retrofit implementation

--- EmergencyEscape/app/src/main/java/com/emergencyescape/retrofit/GetMapApi.java
+++ EmergencyEscape/app/src/main/java/com/emergencyescape/retrofit/GetMapApi.java
@@ -3,10 +3,14 @@ package com.emergencyescape.retrofit;
  * Created by Valerio Mattioli on 20/05/2016.
  */
 
+
+import com.emergencyescape.MapResponse;
+
 import java.util.List;
 
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * com.emergencyescape
@@ -14,8 +18,5 @@ import retrofit2.http.Query;
  */
 public interface GetMapApi { //TODO: Implementare in Retrofit RxJava
     @GET("/progetto/maps")
-    vObservable<List<MapServer>> mapsList(
-        @Query("token") String userToken,
-        Callback<MapData> callback;
-        );
+    Observable<MapResponse> getMaps(); //Ottieni elenco mappe disponibili
 }
